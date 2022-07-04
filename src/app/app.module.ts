ed9e1bd172c8880741cfd0ac68227f73eab0f903
ezice <rhuff2@yahoo.com>
2022-07-03 21:29:50 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    NavComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
